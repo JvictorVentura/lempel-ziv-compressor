61b2f99a41b4c39c15061da951d56036eb09d79f
J. Victor R. Ventura <joaovventura@protonmail.com>
2020-07-04 01:48:53 -0400

    added writeLocationAndLength(), searchOnDictionary() and compress().

--- lempel-ziv.c
+++ lempel-ziv.c
@@ -32,7 +32,10 @@ void fillBuffer(char *buffer, FILE *arq, unsigned *bufferFilled){
 
 }
 
-void scrollWindow(char *window, unsigned *windowFilled, unsigned scrollSize, char *buffer){
+void scrollWindow(char *window, unsigned *windowFilled, unsigned scrollSize, char *buffer, unsigned bufferFilled){
+	if(scrollSize > bufferFilled)
+		scrollSize = bufferFilled;
+
 	int k = 0;
 	int i = scrollSize;
 
@@ -57,92 +60,154 @@ void scrollBuffer(char *buffer, unsigned *sizeOfBuffer, unsigned scrollSize, FIL
 		buffer[i - scrollSize] = buffer[i];
 	}
 
-	*sizeOfBuffer -= scrollSize;
+	
+	if(scrollSize > *sizeOfBuffer)
+		*sizeOfBuffer = 0;
+	else
+		*sizeOfBuffer -= scrollSize;
 
 	fillBuffer(buffer, arq, sizeOfBuffer);
 }
 
 
-int main(){
-	char buffer[BUFFER_SIZE];
-	char window[WINDOW_SIZE];
+byte searchOnDictionary(char *window, char *buffer, unsigned *bufferFilled, unsigned *windowFilled, unsigned *length, unsigned *location){
+	unsigned longestSequence = 0;
+	unsigned sequenceLocation = 0;
+
+	unsigned actualSequence = 0;
+	unsigned actualLocation = 0;
+
+	int i2 = 0;
+
+	for(int i = 0 ; i < *windowFilled; ++i){
+
+		if( i2 == (BUFFER_SIZE -1) )
+			break;
+
+		if(actualSequence == 0){
+			if(window[i] == buffer[i2]){
+				actualLocation = i;
+				++actualSequence;
+				++i2;
+			}
+		}else{
+			if(window[i] == buffer[i2]){
+				++actualSequence;
+				++i2;
+			}else{
+				if(actualSequence > longestSequence){
+					longestSequence = actualSequence;
+					sequenceLocation = actualLocation;
+				}
+				actualSequence = 0;
+				i2 = 0;
+			}
+		}
+
+	}	
+
+	if(actualSequence > longestSequence){
+		longestSequence = actualSequence;
+		sequenceLocation = actualLocation;
+	}
+
+	*length = longestSequence;
+	*location = sequenceLocation;
+
+	if(longestSequence > 0)
+		return 1;
+	else
+		return 0;
+}
+
+void write_location_and_length(unsigned length, unsigned location, FILE *arq){
+	byte byteWritten = 0;
+	for(int i = 16; i > 0; i >>= 1){
+		byteWritten += length & i;
+	}
+
+	byteWritten <<= 3;
+	int index = 4;
+
+	for(int i = 1024; i > 0; i >>= 1){
+
+		if(index == 0){
+			fputc(byteWritten, arq);
+			index = 128;
+			byteWritten = 0;
+		}
+
+		if( (location & i) == i){
+			byteWritten = index;
+		}
+
+		index >>= 1;
+	}
+
+	fputc(byteWritten, arq);
+}
+
+void compress(char *filename){
+
+	char *buffer = malloc(BUFFER_SIZE);
+	char *window = malloc(WINDOW_SIZE);
 	unsigned bufferFilled = 0;
 	unsigned windowFilled = 0;
-	unsigned scrollSize = 1;
 
-	FILE *arq = fopen("fileTest", "r");
+	unsigned length = 0;
+	unsigned location = 0;
+
+	FILE *arq = fopen(filename, "r");
 
 	if(arq != NULL){
 		fillBuffer(buffer, arq, &bufferFilled);
 	}
 
-	scrollWindow(window, &windowFilled, scrollSize, buffer);
-	scrollBuffer(buffer, &bufferFilled, scrollSize, arq);
-
-	printWindow(window, windowFilled);
-	printf("\t\\\t");
-	printBuffer(buffer, bufferFilled);
-
-//-------------------------------------------------------------
-	scrollSize = 1;
-
-	scrollWindow(window, &windowFilled, scrollSize, buffer);
-	scrollBuffer(buffer, &bufferFilled, scrollSize, arq);
-
-	printf("\n\n");
-	printWindow(window, windowFilled);
-	printf("\t\\\t");
-	printBuffer(buffer, bufferFilled);
+	FILE *arq2 = fopen("fileCompressed.lz", "w");
 
 
-//-------------------------------------------------------------
-	scrollSize = 2;
+	while(bufferFilled > 0){
 
-	scrollWindow(window, &windowFilled, scrollSize, buffer);
-	scrollBuffer(buffer, &bufferFilled, scrollSize, arq);
+		/*
+		printf("\n");
+		printWindow(window, windowFilled);
+		printf("\t|\t");
+		printBuffer(buffer, bufferFilled);
 
-	printf("\n\n");
-	printWindow(window, windowFilled);
-	printf("\t\\\t");
-	printBuffer(buffer, bufferFilled);
+		printf("\t (%d,%d)\n", length, location);
+		*/
 
-//--------------------------------------------------------------
+		if(searchOnDictionary(window, buffer, &bufferFilled, &windowFilled, &length, &location)){
 
-	scrollSize = 3;
+			write_location_and_length(length, location, arq2);
+			if(length < bufferFilled)
+				fputc(buffer[length], arq2);
 
-	scrollWindow(window, &windowFilled, scrollSize, buffer);
-	scrollBuffer(buffer, &bufferFilled, scrollSize, arq);
+			scrollWindow(window, &windowFilled, length + 1, buffer, bufferFilled);
+			scrollBuffer(buffer, &bufferFilled, length + 1, arq);
+		
+		}else{
+			fputc(0, arq2);
+			if(length < bufferFilled)
+				fputc(buffer[length], arq2);
 
-	printf("\n\n");
-	printWindow(window, windowFilled);
-	printf("\t\\\t");
-	printBuffer(buffer, bufferFilled);
+			scrollWindow(window, &windowFilled, 1, buffer, bufferFilled);
+			scrollBuffer(buffer, &bufferFilled, 1, arq);
+		}
 
-//---------------------------------------------------------------
 
-	scrollSize = 3;
+	}
 
-	scrollWindow(window, &windowFilled, scrollSize, buffer);
-	scrollBuffer(buffer, &bufferFilled, scrollSize, arq);
-
-	printf("\n\n");
-	printWindow(window, windowFilled);
-	printf("\t\\\t");
-	printBuffer(buffer, bufferFilled);
-
-//---------------------------------------------------------------
-
-	scrollSize = 3;
-
-	scrollWindow(window, &windowFilled, scrollSize, buffer);
-	scrollBuffer(buffer, &bufferFilled, scrollSize, arq);
-
-	printf("\n\n");
-	printWindow(window, windowFilled);
-	printf("\t\\\t");
-	printBuffer(buffer, bufferFilled);
-
+	free(buffer);
+	free(window);
 	fclose(arq);
+	fclose(arq2);
+}
+
+int main(){
+	
+	compress("fileTest");
 
 	return 0;
+	
 }
