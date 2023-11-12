946a68993f5841c14d73aa602f3bcb52782ec6a4
JvictorVentura <joaovventura@protonmail.com>
2023-11-11 23:39:07 -0400

    added arguments to the program. The compressor is not working for some reason. I thought i had finished. Maybe i lost the finished version.

--- lempel-ziv.c
+++ lempel-ziv.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #define WINDOW_SIZE 8/*2048*/
 #define BUFFER_SIZE 4/*29*/
+#define MAX_FILENAME_LENGTH 55
 #include <stdint.h>
+#include <string.h>
+//enum Command { COMPRESS, DECOMPRESS};
 
 void printWindow(char *window, uint16_t sizeOfWindowFilled){
 	for(uint16_t i = 0; i < sizeOfWindowFilled; ++i)
@@ -17,12 +20,12 @@ void printBuffer(char *buffer , uint16_t sizeOfBuffer){
 }
 
 
-void fillBuffer(char *buffer, FILE *arq, uint16_t *bufferFilled){
+void fillBuffer(char *buffer, FILE *base_file, uint16_t *bufferFilled){
 
 	int c = ' ';
 	
 	for(uint16_t i = *bufferFilled; i < BUFFER_SIZE; ++i){
-		if((c = fgetc(arq))== EOF)
+		if((c = fgetc(base_file))== EOF)
 			break;
 		else{
 			buffer[i] = c;
@@ -55,7 +58,7 @@ void scrollWindow(char *window, uint16_t *windowFilled, uint16_t scrollSize, cha
 	
 }
 
-void scrollBuffer(char *buffer, uint16_t *sizeOfBuffer, uint16_t scrollSize, FILE *arq){
+void scrollBuffer(char *buffer, uint16_t *sizeOfBuffer, uint16_t scrollSize, FILE *base_file){
 	for (int i = scrollSize; i < *sizeOfBuffer; ++i){
 		buffer[i - scrollSize] = buffer[i];
 	}
@@ -66,7 +69,7 @@ void scrollBuffer(char *buffer, uint16_t *sizeOfBuffer, uint16_t scrollSize, FIL
 	else
 		*sizeOfBuffer -= scrollSize;
 
-	fillBuffer(buffer, arq, sizeOfBuffer);
+	fillBuffer(buffer, base_file, sizeOfBuffer);
 }
 
 
@@ -120,7 +123,7 @@ uint8_t searchOnDictionary(char *window, char *buffer, uint16_t *bufferFilled, u
 		return 0;
 }
 
-void write_location_and_length(uint16_t length, uint16_t location, FILE *arq){
+void write_location_and_length(uint16_t length, uint16_t location, FILE *base_file){
 	uint8_t uint8_tWritten = 0;
 	for(int i = 16; i > 0; i >>= 1){
 		uint8_tWritten += length & i;
@@ -132,7 +135,7 @@ void write_location_and_length(uint16_t length, uint16_t location, FILE *arq){
 	for(int i = 1024; i > 0; i >>= 1){
 
 		if(index == 0){
-			fputc(uint8_tWritten, arq);
+			fputc(uint8_tWritten, base_file);
 			index = 128;
 			uint8_tWritten = 0;
 		}
@@ -144,10 +147,36 @@ void write_location_and_length(uint16_t length, uint16_t location, FILE *arq){
 		index >>= 1;
 	}
 
-	fputc(uint8_tWritten, arq);
+	fputc(uint8_tWritten, base_file);
 }
 
-void compress(char *filename){
+char *add_extension_filename(char *current_filename){
+	uint8_t filename_length = strlen(current_filename);
+	printf("p1\n");
+	int8_t dot_position = -1;		//-1 means it does not have a dot on the filename
+	
+	for(uint8_t i = 0; i < filename_length; ++i){
+		if(current_filename[i] == '.'){
+			dot_position = i;
+			break;
+		}
+	}
+
+	static char new_filename[MAX_FILENAME_LENGTH];
+
+	if(dot_position >= 1){
+		strncpy(new_filename, current_filename, dot_position);
+	}else{
+		strcpy(new_filename, current_filename);
+	}
+	
+	strcat(new_filename, ".lz");
+
+	return new_filename;
+
+}
+
+void compress(char *file_to_be_compressed, char *name_of_compressed_file){
 
 	char *buffer = malloc(BUFFER_SIZE);
 	char *window = malloc(WINDOW_SIZE);
@@ -157,13 +186,27 @@ void compress(char *filename){
 	uint16_t length = 0;
 	uint16_t location = 0;
 
-	FILE *arq = fopen(filename, "r");
+	FILE *base_file = fopen(file_to_be_compressed, "r");
 
-	if(arq != NULL){
-		fillBuffer(buffer, arq, &bufferFilled);
+	if(base_file != NULL){
+		fillBuffer(buffer, base_file, &bufferFilled);
+	}else{
+		printf("Arquivo não existe\n");
+		return;
 	}
 
-	FILE *arq2 = fopen("fileCompressed.lz", "w");
+	printf("%s\n\n", name_of_compressed_file);	
+	FILE *compressed_file = NULL; 
+	if(name_of_compressed_file != NULL){
+		compressed_file = fopen(name_of_compressed_file, "w");
+	}else{
+		compressed_file = fopen(add_extension_filename(file_to_be_compressed), "w");
+	}
+
+	if(compressed_file == NULL){
+		printf("Erro ao criar base_fileuivo\n");
+			return;
+	}
 
 
 	while(bufferFilled > 0){
@@ -179,20 +222,20 @@ void compress(char *filename){
 
 		if(searchOnDictionary(window, buffer, &bufferFilled, &windowFilled, &length, &location)){
 
-			write_location_and_length(length, location, arq2);
+			write_location_and_length(length, location, compressed_file);
 			if(length < bufferFilled)
-				fputc(buffer[length], arq2);
+				fputc(buffer[length], compressed_file);
 
 			scrollWindow(window, &windowFilled, length + 1, buffer, bufferFilled);
-			scrollBuffer(buffer, &bufferFilled, length + 1, arq);
+			scrollBuffer(buffer, &bufferFilled, length + 1, base_file);
 		
 		}else{
-			fputc(0, arq2);
+			fputc(0, compressed_file);
 			if(length < bufferFilled)
-				fputc(buffer[length], arq2);
+				fputc(buffer[length], compressed_file);
 
 			scrollWindow(window, &windowFilled, 1, buffer, bufferFilled);
-			scrollBuffer(buffer, &bufferFilled, 1, arq);
+			scrollBuffer(buffer, &bufferFilled, 1, base_file);
 		}
 
 
@@ -200,13 +243,33 @@ void compress(char *filename){
 
 	free(buffer);
 	free(window);
-	fclose(arq);
-	fclose(arq2);
+	fclose(base_file);
+	fclose(compressed_file);
 }
 
-int main(){
+// WTF i thought i implemented the decompressed function, maybe i lost that version, damn
+void decompress(char *file_to_be_decompressed, char *name_of_decompressed_file){
+
+}
+
+int main(int argc, char *argv[]){
+	if(argc <= 2)		printf("Comando inválido\n");
+	else{
+
+		if(strcmp(argv[1], "-c") == 0){
+			if(argc > 3)
+				compress(argv[2], argv[3]);
+			else
+				compress(argv[2], NULL);
+		}
+
+		if(strcmp(argv[1], "-d") == 0){
+			//todo
+		}
+
+	}
+
 	
-	compress("fileTest");
 
 	return 0;
 	
